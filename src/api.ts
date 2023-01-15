c62bd9c28bd7fb4f78457255e74dc4048abfab64
beom-yun <scds0326@naver.com>
2023-01-16 00:35:57 +0900

    Home menu, score detail

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,8 @@ const instance = axios.create({
   baseURL: 'http://127.0.0.1:8000/api/v1/',
 });
 
+export const getRegularGameDates = () => instance.get('scores/').then(response => response.data);
+
 export const getRegularGame = ({ queryKey }: QueryFunctionContext) => {
   const [_, regularGamePk] = queryKey;
   return instance.get(`scores/${regularGamePk}/`).then(response => response.data);
--- src/routes/RegularGameDetail.tsx
+++ src/routes/RegularGameDetail.tsx
@@ -1,9 +1,67 @@
+import { Heading, Table, TableCaption, TableContainer, Tbody, Td, Text, Th, Thead, Tr } from '@chakra-ui/react';
 import { useQuery } from '@tanstack/react-query';
 import { useParams } from 'react-router-dom';
 import { getRegularGame } from '../api';
 
+interface IBowler {
+  name: string;
+}
+
+interface IRegularGameScore {
+  bowler: IBowler;
+  first: number;
+  second: number;
+  third: number;
+  fourth: number;
+  total_score: number;
+  game_count: number;
+  average: number;
+  last_average: number;
+  average_change: number;
+  high_low: number;
+}
+
 export default function RegularGameDetail() {
   const { regularGamePk } = useParams();
-  const { isLoading, data } = useQuery(['regularGame', regularGamePk], getRegularGame);
-  return <span>regular game {regularGamePk} detail</span>;
+  const { isLoading, data } = useQuery<IRegularGameScore[]>(['regularGame', regularGamePk], getRegularGame);
+
+  return (
+    <TableContainer>
+      <Table size={'md'} variant={'simple'}>
+        <TableCaption placement="top" mb={4}>
+          <Heading>정기전 {regularGamePk}회차</Heading>
+        </TableCaption>
+        <Thead>
+          <Tr>
+            <Th>이름</Th>
+            <Th>전회평균</Th>
+            <Th>평균</Th>
+            <Th>변동</Th>
+            <Th>총점</Th>
+            <Th>1게임</Th>
+            <Th>2게임</Th>
+            <Th>3게임</Th>
+            <Th>4게임</Th>
+            <Th>편차</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {data?.map(score => (
+            <Tr>
+              <Td>{score.bowler.name}</Td>
+              <Td>{score.last_average}</Td>
+              <Td>{score.average}</Td>
+              <Td>{score.average_change}</Td>
+              <Td>{score.total_score}</Td>
+              <Td>{score.first}</Td>
+              <Td>{score.second}</Td>
+              <Td>{score.third}</Td>
+              <Td>{score.fourth}</Td>
+              <Td>{score.high_low}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
+    </TableContainer>
+  );
 }
