d8e15cd02999467400d9dec0989229f0b65ef13f
beom-yun <58946778+beom-yun@users.noreply.github.com>
2023-09-06 09:48:56 +0900

    Signup kakao

--- src/components/SignupModal.tsx
+++ src/components/SignupModal.tsx
@@ -16,6 +16,13 @@ interface SignupModalProps {
 }
 
 export default function SignupModal({ isOpen, onClose }: SignupModalProps) {
+  const kakaoParams = {
+    client_id: '06acaa0ad678eefcbb78902fec710ad6',
+    redirect_uri: 'http://127.0.0.1:3000/social/kakao/',
+    response_type: 'code',
+  };
+  const params = new URLSearchParams(kakaoParams).toString();
+
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <ModalOverlay />
@@ -25,7 +32,14 @@ export default function SignupModal({ isOpen, onClose }: SignupModalProps) {
         </ModalHeader>
         <ModalCloseButton />
         <ModalBody>
-          <Button my={4} w={'100%'} colorScheme="yellow" leftIcon={<FaComment />}>
+          <Button
+            as={'a'}
+            href={`https://kauth.kakao.com/oauth/authorize?${params}`}
+            my={4}
+            w={'100%'}
+            colorScheme="yellow"
+            leftIcon={<FaComment />}
+          >
             카카오로 가입하기
           </Button>
         </ModalBody>
--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,3 +1,11 @@
+import { Heading, VStack } from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <VStack>
+      <Heading>Home</Heading>
+      <Link to={'/'}>레코드</Link>
+    </VStack>
+  );
 }
